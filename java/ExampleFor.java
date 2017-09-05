1dcf6b7ee80d555c5a85ac2788f9fec07ac41651
Sheng-Liang Song <sheng-liang.song@sjsu.edu>
2017-09-05 12:48:21 -0700

    Adds ExmpleFor.java

---
+++ java/ExampleFor.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+import java.net.URLDecoder;
+
+public class ExampleFor {
+
+    public static void main(String[] args) {
+ 
+            Scanner sc=new Scanner(System.in);
+	    try {
+		    String path =  ExampleFor.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		    String decodedPath = URLDecoder.decode(path, "UTF-8");
+		    // String className = this.getClass().getSimpleName(); 
+		    String className = new Object(){}.getClass().getEnclosingClass().getName();
+	            // int n=sc.nextInt();
+		    int n = 1;
+	            for(int j=0; j<n; j++) {
+	                  System.out.printf(decodedPath + className);
+	            }
+	            System.out.printf("\n");
+		    throw new UnsupportedEncodingException("ME");
+
+	    } catch (UnsupportedEncodingException e) {
+	    	System.out.printf("Failed to decode:" + e.getMessage(), e);
+	        e.printStackTrace();
+	    }
+    }
+}
