6dfba51174aa69af827f157a42dc3b4626b3b7fa
Sheng-Liang Song <sheng-liang.song@sjsu.edu>
2018-01-22 15:29:58 -0800

    Added ex2.cpp with gdb disassemble output

---
+++ c++/boost_example/ex2.cpp
@@ -0,0 +1,70 @@
+/*
+ * Reference:
+ * CppCon 2017: Louis Brandy “Curiously Recurring C++ Bugs at Facebook”
+ * https://www.youtube.com/watch?v=lkgszkPnV8g
+ * http://en.cppreference.com/w/cpp/thread/scoped_lock
+ */
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+#include <boost/bind/placeholders.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/function.hpp>
+#include <boost/thread.hpp>
+ 
+typedef boost::mutex mutex_t;
+typedef boost::mutex::scoped_lock scoped_lock_t;
+
+class AInt {
+   int m_i;
+   mutex_t m_mutex;
+public:
+   AInt() : m_i(0), m_mutex() { }
+
+   void bug1_inc() {
+      scoped_lock_t(this->m_mutex);
+      ++m_i;
+   }
+   void bug2_inc() {
+      scoped_lock_t(m_mutex);
+      ++m_i;
+   }
+   void ok_inc() {
+      scoped_lock_t lock(m_mutex);
+      ++m_i;
+      std::cout << std::this_thread::get_id() << ": " << m_i << '\n';
+   }
+   int& operator++() {
+      bug1_inc();
+      bug2_inc();
+      ok_inc();
+      return m_i;
+   }
+   int& operator--() {  scoped_lock_t lock(this->m_mutex); --m_i; return m_i;}
+   int get() const { return m_i; }
+   // friend std::ostream& operator<<(std::ostream& out, const AInt& a);
+};
+
+std::ostream& operator<<(std::ostream& out, const AInt& a) {
+	return out << a.get();
+}
+
+AInt g_x;
+void thread_main()
+{
+    ++g_x;
+}
+ 
+int main(void)
+{
+    std::cout << __func__ << ": " << g_x << '\n';
+    std::thread t1(thread_main);
+    std::thread t2(thread_main);
+    t1.join();
+    t2.join();
+    std::cout << __func__ << ": " << g_x << '\n';
+    return 0;
+}
+
