7d7d3453ab7e8f1c4e5d5fd672440a7a14294208
Sheng-Liang Song <sheson@microsoft.com>
2020-10-05 14:25:09 -0700

    push/pop unique_ptr from queue

--- c++/make_unique.cpp
+++ c++/make_unique.cpp
@@ -1,7 +1,5 @@
 
-#include <iostream>
-#include <memory>
-
+#include<bits/stdc++.h>
 
 struct my_deleter {
   template <class T>
@@ -34,8 +32,61 @@ struct Vec3
     }
 };
 
-int main()
+struct request_t {
+  int id;
+  int len;
+  char *data;
+  request_t(int _id, int _len, const char* _data) {
+	  id = _id;
+	  len = _len;
+	  data = new char[len];
+	  memcpy(data, _data, len);
+	  std::cout << __FUNCTION__ << " " << id <<  std::endl;
+  }
+  ~request_t(void) {
+	  std::cout << __FUNCTION__ << " " << id << std::endl;
+	  if(data) {
+		  delete [] data;
+		  data = nullptr;
+	  }
+  }
+};
+
+class request_hdl_t {
+	std::queue<std::unique_ptr<request_t>> q;
+	public:
+	request_hdl_t() { }
+	void push(std::unique_ptr<request_t> p) {
+		q.push(std::move(p));
+	}
+	std::unique_ptr<request_t> pop(void) {
+		auto rc = std::move(q.front());
+		q.pop();
+		return std::move(rc);
+	}
+	int size() {
+		return q.size();
+	}
+	bool empty() {
+		return q.empty();
+	}
+};
+
+std::unique_ptr<request_hdl_t>  request_hdl;
+
+
+int main(void)
 {
+	request_hdl.reset(new request_hdl_t());
+	for(int i = 0; i < 8; ++i) {
+		std::string data = "hello";
+		request_hdl->push(std::make_unique<request_t>(i, data.length(), data.c_str()));
+	}
+	while(!request_hdl->empty()) {
+	    auto e = request_hdl->pop();
+	    std::cout << e->id << " " << e->len << " " << e->data << std::endl;
+	}
+
     // Use the default constructor.
     std::unique_ptr<Vec3> v1 = std::make_unique<Vec3>();
     // Use the constructor that matches these arguments
