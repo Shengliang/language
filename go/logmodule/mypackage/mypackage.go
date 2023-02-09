17cabf2bf01bccfbea6cd347dbf84a5ca8c15f11
Sheng-Liang Song <shengliang.song@bytedance.com>
2023-02-09 14:59:36 -0800

    Add goroutine example

--- go/logmodule/mypackage/mypackage.go
+++ go/logmodule/mypackage/mypackage.go
@@ -1,11 +1,16 @@
 
 
+// Ref: https://gobyexample.com/waitgroups
+
 package mypackage
 
-// Importing fmt package for the sake of printing
 import (
     "fmt"
+    "time"
+    "sync"
+    "sync/atomic"
     "context"
+    "strconv"
     "os"
     "github.com/go-logr/logr"
     "github.com/go-logr/zerologr"
@@ -30,10 +35,13 @@ type Animaler interface {
 	Speak()
 	Error()
 	Log() logr.Logger
+	Run(int, logr.Logger)
+	Schedule()
 }
 
 // A struct holding a string variable: SuperAnimals
 type SuperAnimals struct {
+	m_ctx context.Context
 	locomotion string
 }
 
@@ -46,12 +54,14 @@ type Animals struct {
     food string
     sound string
     log logr.Logger
-    cnt int
+    messages chan int
+    cnt uint64
+    run_cnt uint64
 }
 
 func (x *Animals) Log() logr.Logger {
-	x.cnt+=1
-     return x.log.WithName(x.locomotion).WithValues("food", x.food).WithValues("sound", x.sound).WithValues("cnt", x.cnt)
+     cnt := atomic.AddUint64(&x.cnt, 1)
+     return x.log.WithName(x.locomotion).WithValues("food", x.food).WithValues("sound", x.sound).WithValues("cnt", cnt)
 }
 
 // Now we are indirectly implementing
@@ -81,40 +91,86 @@ func (x *Animals) Error() {
 	fmt.Println("Invalid query entered!")
 }
 
+func (x *Animals) Run(id int, Log2 logr.Logger) {
+    Log2.Info("RUN_BEGIN")
+   //var Log logr.Logger = logr.FromContextOrDiscard(x.m_ctx).WithName("Run").WithValues("RUN_CNT", x.run_cnt)
+   var Log logr.Logger = x.Log().WithName("Run").WithValues("RUN_CNT", x.run_cnt)
+   run_cnt_id := atomic.AddUint64(&x.run_cnt, 1)
+   for w := 0; w < 2; w++ {
+	Log.Info(strconv.FormatUint(run_cnt_id, 10), "logr", fmt.Sprintf("%p", &Log), "ID", id, "w", w);
+        time.Sleep(100 * time.Millisecond)
+    }
+    x.messages <- id
+    Log2.Info("RUN_END")
+}
+func (x *Animals) Schedule() {
+        //var Log logr.Logger = logr.FromContextOrDiscard(x.m_ctx).WithName("Schedule")
+        var Log logr.Logger = x.Log().WithName("Schedule")
+	var wg sync.WaitGroup
+	Log.Info("Enter", "BeginTime", time.Now())
+	defer func() {
+		Log.Info("Exit", "EndTime", time.Now())
+	}()
+	for id := 0; id < 2; id++ {
+            wg.Add(1)
+	    go func(id int) {
+		 defer wg.Done()
+		 x.Run(id, Log.WithValues("id", id))
+	    }(id)
+	}
+
+	//time.Sleep(1*time.Second);
+        go func(){
+          Log.Info("wait...")
+	  for id := range x.messages {
+             Log.Info("got", "id", id)
+          }
+        }()
+
+	wg.Wait()
+
+}
+
 func Foo() {
     ctx := context.Background()
     zl := zerolog.New(os.Stderr).Level(zerolog.DebugLevel)
     logger := zerologr.New(&zl);
-    ctx = logr.NewContext(ctx, logger.WithName("helper").WithValues("pkg", "mypackage"))
+    ctx = logr.NewContext(ctx, logger.WithName("Foo").WithValues("pkg", "mypackage"))
     helper(ctx)
 
     m := map[string]Animaler{
 	    "cow":
          &Animals{
                   SuperAnimals: SuperAnimals{
+                     m_ctx : ctx,
                      locomotion: "walk",
                   },
                   food: "grass",
                   sound: "moo",
                   log: logr.FromContextOrDiscard(ctx),
+		  messages: make(chan int),
          },
 	    "brid":
          &Animals{
                   SuperAnimals: SuperAnimals{
+                     m_ctx : ctx,
                      locomotion: "fly",
                   },
                   food: "worms",
                   sound: "peep",
                   log: logr.FromContextOrDiscard(ctx),
+		  messages: make(chan int),
          },
 	    "snake":
          &Animals{
                   SuperAnimals: SuperAnimals{
+                     m_ctx : ctx,
                      locomotion: "slither",
                   },
                   food: "mice",
                   sound: "hsss",
                   log: logr.FromContextOrDiscard(ctx),
+		  messages: make(chan int),
          },
     }
     var animal string
@@ -122,12 +178,14 @@ func Foo() {
     m[animal].Eat()
     m[animal].Move()
     m[animal].Speak()
-    m[animal].Log().V(1).Info("msg", "x", 41)
-    m[animal].Log().V(1).Info("msg", "y", 107)
-    m[animal].Log().V(1).Info("msg", "z", 314)
+    m[animal].Log().V(1).WithName("X").Info("msg", "x", 141)
+    m[animal].Log().V(1).WithName("Y").Info("msg", "y", 107)
+    m[animal].Log().V(1).WithName("Z").Info("msg", "z", 314)
     animal = "brid"
     m[animal].Log().V(0).Info("msg", "x", 42)
     animal = "snake"
     m[animal].Log().Info("msg", "x", 43)
+    m[animal].Schedule()
+
 }
 
