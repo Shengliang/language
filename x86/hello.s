e1346cc637177ec5c2535357b52361088001aac7
Sheng-Liang Song <shengliang.song@bytedance.com>
2023-02-21 16:51:43 -0800

    Add x86 ASM example

---
+++ x86/hello.s
@@ -0,0 +1,21 @@
+# https://cs.lmu.edu/~ray/notes/gasexamples/
+# gcc -c hello.s && ld -static hello.o && ./a.out
+# as hello.s -o hello.o && ld -static hello.o && ./a.out
+
+        .global _start
+
+        .text
+_start:
+        # write(1, message, 13)
+        mov     $1, %rax                # system call 1 is write
+        mov     $1, %rdi                # file handle 1 is stdout
+        mov     $message, %rsi          # address of string to output
+        mov     $13, %rdx               # number of bytes
+        syscall                         # invoke operating system to do the write
+
+        # exit(0)
+        mov     $60, %rax               # system call 60 is exit
+        xor     %rdi, %rdi              # we want return code 0
+        syscall                         # invoke operating system to exit
+message:
+        .ascii  "Hello, world\n"
